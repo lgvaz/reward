7ce6faf6f612d1ad2e71c2ac5f3b1bdf7d1b8050
lgvaz <lucasgouvaz@gmail.com>
2018-01-13 00:57:53 -0200

    adaptive input

--- torchrl/models/base_model.py
+++ torchrl/models/base_model.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
+from collections import OrderedDict
 
 import torch
 import torch.nn as nn
 
-from torchrl.nn import ModuleExtended
-from torchrl.utils import Config
+from torchrl.nn import ModuleExtended, SequentialExtended
+from torchrl.utils import Config, get_obj
 
 
 class BaseModel(ModuleExtended, ABC):
@@ -22,15 +23,15 @@ class BaseModel(ModuleExtended, ABC):
         If True and cuda is supported, use it.
     '''
 
-    def __init__(self, nn_body, nn_head, cuda_default=True):
+    def __init__(self, config, cuda_default=True):
         super().__init__()
 
         self.num_updates = 0
 
         assert isinstance(nn_body, Config) and isinstance(nn_head, Config), \
             'nn_bodies and nn_heads must be of type {}'.format(Config.__name__)
-        self.nn_body = nn_body
-        self.nn_head = nn_head
+        self.nn_body = config.nn_body
+        self.nn_head = config.nn_head
 
         self.cuda_enabled = cuda_default and torch.cuda.is_available()
         if self.cuda_enabled:
@@ -139,13 +140,40 @@ class BaseModel(ModuleExtended, ABC):
         torchrl.models
             A TorchRL model.
         '''
-        nn_body = config.model.nn_body.obj.from_config(
-            config.model.nn_body.arch.as_dict(),
-            kwargs=config.model.nn_body.kwargs.as_dict())
-        nn_head = config.model.nn_head.obj(
-            input_shape=nn_body.get_output_shape(state_shape),
-            output_shape=action_shape,
-            **config.model.nn_head.kwargs.as_dict())
+        # Prepare for creating body network dict
+        body_dict = OrderedDict()
+        body_configs = iter(config.nn_body.arch.items())
+
+        # Get first layer
+        key, obj_config = next(body_configs)
+        assert 'Input' in obj_config['func'].__name__, \
+            'The first layer of the network must be an Input layer'
+        obj_config['input_shape'] = state_shape
+        body_dict[key] = get_obj(obj_config)
+
+        # Get other layers
+        for key, obj_config in body_configs:
+            body_dict[key] = get_obj(obj_config)
+        # Encapsulate all layers
+        nn_body = SequentialExtended(body_dict)
+
+        # Prepare for creating head network dict
+        head_dict = OrderedDict()
+        head_configs = iter(config.nn_head.arch.items())
+
+        # Get first layer
+        key, obj_config = next(head_configs)
+        assert 'Input' in obj_config['func'].__name__, \
+            'The first layer of the network must be an Input layer'
+        obj_config['input_shape'] = nn_body.get_output_shape(state_shape)
+        head_dict[key] = get_obj(obj_config)
+
+        # TODO: Output is also dynamic!!
+        # Get other layers
+        for key, obj_dict in head_configs:
+            head_dict[key] = get_obj(obj_dict)
+        # Encapsulate all layers
+        nn_head = SequentialExtended(body_dict)
 
         return cls(nn_body=nn_body, nn_head=nn_head, **config.model.kwargs.as_dict())
 
