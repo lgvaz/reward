8b6ce80a74d73e1b6a1e1217cf4b4df206341368
lgvaz <lucasgouvaz@gmail.com>
2018-04-16 19:35:11 -0300

    deleted datasets.py

--- torchrl/utils/__init__.py
+++ torchrl/utils/__init__.py
@@ -3,7 +3,6 @@ from .config import Config
 from .utils import get_obj, env_from_config, to_numpy, explained_var, normalize
 from .logger import Logger
 from .net_builder import auto_input_shape, get_module_list, nn_from_config
-from .datasets import BasicDataset, DataGenerator
 from .memories import Batch, SimpleMemory
 
 import torchrl.utils.estimators
@@ -11,5 +10,5 @@ import torchrl.utils.estimators
 __all__ = [
     'EPSILON', 'Config', 'Logger', 'get_obj', 'env_from_config', 'to_numpy',
     'explained_var', 'normalize', 'auto_input_shape', 'get_module_list', 'nn_from_config',
-    'BasicDataset', 'DataGenerator', 'Batch', 'SimpleMemory'
+    'Batch', 'SimpleMemory'
 ]
