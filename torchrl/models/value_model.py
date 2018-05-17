097f2ded89ce4d4f0a709c0324c2dca5b61b5f2b
Lucas Vazquez <lucasgouvaz@gmail.com>
2018-05-17 19:52:13 -0300

    Add clip option to default value model

--- torchrl/models/value_model.py
+++ torchrl/models/value_model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn.functional as F
 import torchrl.utils as U
 from torchrl.models import BaseModel
@@ -15,43 +16,74 @@ class ValueModel(BaseModel):
         The mini-batch size (Default is 64).
     num_epochs: int
         How many times to train over the entire dataset (Default is 10).
+    clip_range: float
+        Similar to PPOClip, limits the change between the new and old value function.
     '''
 
-    def __init__(self, model, env, batch_size=64, num_epochs=10, **kwargs):
+    def __init__(self,
+                 model,
+                 env,
+                 batch_size=64,
+                 num_epochs=10,
+                 clip_range=None,
+                 **kwargs):
         super().__init__(model=model, env=env, **kwargs)
         self.batch_size = batch_size
         self.num_epochs = num_epochs
+        self.clip_range = clip_range
 
-    @property
-    def loss_fn(self):
-        return F.mse_loss
-        # return F.smooth_l1_loss
+        assert clip_range is None or clip_range > 0, 'clip_range must be None or > 0'
 
-    def add_losses(self, states, vtargets):
-        preds = self.forward(states).view(-1)
-        loss = self.loss_fn(preds, vtargets)
+    def mse_loss(self, batch):
+        pred = self.forward(batch.state_t).view(-1)
+        loss = F.mse_loss(pred, batch.vtarget)
+
         self.losses.append(loss)
+
+    def clipped_mse_loss(self, batch):
+        pred = self.forward(batch.state_t).view(-1)
+        pred_diff = pred - batch.old_pred
+        pred_clipped = batch.old_pred + pred_diff.clamp(-self.clip_range, self.clip_range)
+
+        losses = (pred - batch.vtarget)**2
+        losses_clipped = (pred_clipped - batch.vtarget)**2
+
+        loss = 0.5 * torch.max(losses, losses_clipped).mean()
+
+        self.losses.append(loss)
+
+        if self.logger is not None:
+            clip_frac = (abs(pred_diff) > self.clip_range).float().mean()
+            self.logger.add_log('Value NN/Clip Fraction', clip_frac)
+
+    def add_losses(self, batch):
+        if self.clip_range is None:
+            self.mse_loss(batch)
+        else:
+            self.clipped_mse_loss(batch)
 
     def train(self, batch):
         batch = batch.apply_to_all(self._to_tensor)
 
-        # EV before update
-        if self.logger is not None:
-            preds = self.forward(batch.state_t)
-            self.logger.add_log('Value NN/Old Explained Var',
-                                U.explained_var(batch.vtarget, preds).item())
+        with torch.no_grad():
+            old_preds = self.forward(batch.state_t).view(-1)
 
-        dataset = TensorDataset(batch.state_t, batch.vtarget)
+        dataset = TensorDataset(batch.state_t, batch.vtarget, old_preds)
         data_loader = DataLoader(dataset, batch_size=self.batch_size, shuffle=True)
 
         for _ in range(self.num_epochs):
-            for states, vtargets in data_loader:
-                loss = self.optimizer_step(states=states, vtargets=vtargets)
+            for state_t, vtarget, old_pred in data_loader:
+                mini_batch = U.Batch(
+                    dict(state_t=state_t, vtarget=vtarget, old_pred=old_pred))
+                loss = self.optimizer_step(mini_batch)
+
                 if self.logger is not None:
                     self.logger.add_log('Value NN/Loss', loss.item(), precision=3)
 
         # Ev after update
         if self.logger is not None:
+            self.logger.add_log('Value NN/Old Explained Var',
+                                U.explained_var(batch.vtarget, old_preds).item())
             preds = self.forward(batch.state_t)
             self.logger.add_log('Value NN/New Explained Var',
                                 U.explained_var(batch.vtarget, preds).item())
