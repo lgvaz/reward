fd4758af906b6301ccf567689787c748a4ef08d7
Lucas Vazquez <lucasgouvaz@gmail.com>
2018-05-18 00:24:48 -0300

    import roboschool on init

--- torchrl/envs/roboschool_env.py
+++ torchrl/envs/roboschool_env.py
@@ -6,6 +6,7 @@ class RoboschoolEnv(GymEnv):
     Support for gym Roboschool.
     '''
 
+    # TODO: Test roboschool import
     def __init__(self, *args, **kwargs):
         import roboschool
         super().__init__(*args, **kwargs)
