81d17adcfc207426a14597091768e898b40b714c
Lucas Vazquez <lucasgouvaz@gmail.com>
2018-05-24 19:14:29 -0300

    fix kl calculation

--- torchrl/distributions/categorical.py
+++ torchrl/distributions/categorical.py
@@ -1,6 +1,12 @@
 import torch
+from torch.distributions import kl
 
 
 class Categorical(torch.distributions.Categorical):
     def log_prob(self, value):
         return super().log_prob(value)[..., None]
+
+
+@kl.register_kl(Categorical, Categorical)
+def _kl_categorical_categorical(p, q):
+    return kl._kl_categorical_categorical(p, q)[..., None]
