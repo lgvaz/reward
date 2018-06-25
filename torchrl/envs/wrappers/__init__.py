9b0c3a213952fc04b4950aeba68f654416d54ef8
Lucas Vazquez <lucasgouvaz@gmail.com>
2018-06-25 14:27:34 -0300

    rewardwrappers as classes

--- torchrl/envs/wrappers/__init__.py
+++ torchrl/envs/wrappers/__init__.py
@@ -2,12 +2,12 @@ from .base_wrapper import BaseWrapper
 from .final_wrapper import FinalWrapper
 from .stats_recorder import StatsRecorder
 from .state_wrapper import BaseStateWrapper, RGB2GRAY, Rescale, HWC2CHW
-from .reward_wrapper import RewardWrapper
+from .reward_wrapper import BaseRewardWrapper
 from .misc_wrappers import EpisodicLife, RandomReset, FireReset, ActionRepeat, DelayedStart
 from .atari_wrapper import AtariWrapper
 
 __all__ = [
-    'BaseWrapper', 'StatsRecorder', 'BaseStateWrapper', 'RewardWrapper'
+    'BaseWrapper', 'StatsRecorder', 'BaseStateWrapper', 'BaseRewardWrapper'
     'EpisodicLife', 'RandomReset', 'ActionRepeat', 'AtariWrapper', 'FinalWrapper',
     'RGB2GRAY', 'Rescale', 'HWC2CHW', 'DelayedStart'
 ]
--- torchrl/envs/wrappers/reward_wrapper.py
+++ torchrl/envs/wrappers/reward_wrapper.py
@@ -1,16 +1,14 @@
+from abc import ABC, abstractmethod
 from torchrl.envs.wrappers import BaseWrapper
 
 
-class RewardWrapper(BaseWrapper):
-    def __init__(self, env, funcs=None):
-        self.funcs = funcs or []
-        self.transformed_rewards = []
-        super().__init__(env=env)
+class BaseRewardWrapper(BaseWrapper, ABC):
+    @abstractmethod
+    def transform(self, reward):
+        pass
 
     def step(self, action):
         state, reward, done, info = self.env.step(action)
-        for func in self.funcs:
-            reward = func(reward)
-        self.transformed_rewards = []
+        reward = self.transform(reward)
 
         return state, reward, done, info
