3fa687488968d1a9bf899ec243866e5d78653dba
Lucas Vazquez <lucasgouvaz@gmail.com>
2018-06-26 23:05:14 -0300

    new example

---
+++ examples/vanilla_pg_simple.py
@@ -0,0 +1,53 @@
+import torch.nn as nn
+
+import torchrl.utils as U
+from torchrl.envs import GymEnv
+from torchrl.runners import SingleRunner, PAACRunner
+from torchrl.batchers import RolloutBatcher
+from torchrl.models import VanillaPGModel, ValueModel
+from torchrl.optimizers import SingleOpt, JointOpt, ListOpt
+from torchrl.agents import PGAgent
+
+# Define networks configs
+policy_nn_config = U.Config(
+    body=[dict(func=nn.Linear, out_features=64),
+          dict(func=nn.Tanh)])
+
+value_nn_config = U.Config(
+    body=[dict(func=nn.Linear, out_features=64),
+          dict(func=nn.Tanh)])
+
+# Create env and runner
+env = GymEnv('CartPole-v1')
+runner = SingleRunner(env)
+
+# Create parallel envs
+# envs = [GymEnv('CartPole-v1') for _ in range(16)]
+# runner = PAACRunner(envs)
+
+# Create batcher
+batcher = RolloutBatcher(runner, batch_size=512)
+
+# Create models
+policy_model_config = U.Config(nn_config=policy_nn_config)
+policy_model = VanillaPGModel.from_config(config=policy_model_config, batcher=batcher)
+pot = SingleOpt(policy_model, opt_params=dict(lr=1e-3))
+
+value_model_config = U.Config(nn_config=value_nn_config)
+value_model = ValueModel.from_config(config=value_model_config, batcher=batcher)
+vot = SingleOpt(value_model, num_epochs=10, num_mini_batches=4, opt_params=dict(lr=1e-3))
+
+opt = ListOpt(optimizers=[pot, vot])
+# opt = JointOpt(model=[policy_model, value_model])
+
+# Create agent
+agent = PGAgent(
+    batcher=batcher,
+    optimizer=opt,
+    policy_model=policy_model,
+    value_model=value_model,
+    # advantage=U.estimators.advantage.Baseline(gamma=0.99),
+    # vtarget=U.estimators.value.CompleteReturn(gamma=0.99),
+    log_dir='logs/vanilla/cp/1p-opt-v7-1')
+
+agent.train(max_steps=1e6, log_freq=10)
