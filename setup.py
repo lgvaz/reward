33b1f18ae793d867f0ee7a1688374abda6546987
Lucas Vazquez <lucasgouvaz@gmail.com>
2018-08-28 11:39:52 -0300

    name change

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="TorchRL",
-    version="0.0.1",
-    description="Reinforcement learning for OpenAI Gym and Pytorch",
-    url="https://github.com/lgvaz/torchrl",
+    name="reward",
+    version="0.0.4",
+    description="Reinforcement Learning library",
+    url="https://github.com/lgvaz/reward",
     author="lgvaz",
     author_email="lucasgouvaz@gmail.com",
     packages=[package for package in find_packages() if package.startswith("torchrl")],
