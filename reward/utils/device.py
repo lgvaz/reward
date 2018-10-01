fe9ac58340321be0641e5e051ac85ae6063e309c
Lucas Vazquez <lucasgouvaz@gmail.com>
2018-10-01 14:34:23 -0300

    added device file

---
+++ reward/utils/device.py
@@ -0,0 +1,11 @@
+import torch
+
+CONFIG = {"device": torch.device("cuda" if torch.cuda.is_available() else "cpu")}
+
+
+def get_device():
+    return CONFIG["device"]
+
+
+def set_device(device):
+    CONFIG["device"] = device
