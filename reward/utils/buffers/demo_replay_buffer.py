b97ee708ab80cc860037da89a868d30498a9bc28
Lucas Vazquez <lucasgouvaz@gmail.com>
2018-10-04 14:35:52 -0300

    pr_demo sched

--- reward/utils/buffers/demo_replay_buffer.py
+++ reward/utils/buffers/demo_replay_buffer.py
@@ -1,21 +1,25 @@
 import numpy as np
 from reward.utils.buffers import PrReplayBuffer
+from reward.utils import make_callable
 
 
 class DemoReplayBuffer(PrReplayBuffer):
     def __init__(
-        self, maxlen, num_envs, *, pr_factor, is_factor, min_pr=0.01, pr_demo=0.3
+        self, maxlen, num_envs, *, pr_factor, is_factor, min_pr=0.01, pr_demo=1.
     ):
         super().__init__(
             maxlen, num_envs, pr_factor=pr_factor, is_factor=is_factor, min_pr=min_pr
         )
-        self.pr_demo = pr_demo
+        self._pr_demo = make_callable(pr_demo)
         self.start_idx = 0
         self.pr_demos = np.zeros(maxlen)
 
+    def get_pr_demo(self, step):
+        return self._pr_demo(step)
+
     def add_sample_demo(self, **kwargs):
         super().add_sample(**kwargs)
-        self.pr_demos[self.current_idx] = self.pr_demo
+        self.pr_demos[self.current_idx] = self.get_pr_demo(step=0)
         self.start_idx += 1
 
     def add_sample(self, **kwargs):
@@ -25,5 +29,6 @@ class DemoReplayBuffer(PrReplayBuffer):
 
     def update_pr(self, idx, pr, step):
         pr = pr.squeeze()
+        self.pr_demos[: self.start_idx] = self.get_pr_demo(step)
         pr = pr + self.get_min_pr(step) + self.pr_demos[idx]
         self.probs[idx] = pr ** self.get_pr_factor(step)
