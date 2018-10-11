0b710b94d1c28e463dd6f5c37c75853b6927f566
Lucas Vazquez <lucasgouvaz@gmail.com>
2018-10-11 14:25:08 -0300

    replay_buffer reset

--- reward/utils/buffers/replay_buffer.py
+++ reward/utils/buffers/replay_buffer.py
@@ -3,42 +3,6 @@ import numpy as np
 from reward.utils import Batch, to_np
 
 
-# class ReplayBuffer:
-#     def __init__(self, maxlen, num_envs):
-#         self.maxlen = maxlen
-#         self.num_envs = num_envs
-#         self.buffer = []
-#         # Intialized at -1 so the first updated position is 0
-#         self.position = -1
-
-#     def __len__(self):
-#         return len(self.buffer) * self.num_envs
-
-#     def __getitem__(self, key):
-#         row, col = key // self.num_envs, key % self.num_envs
-#         return {k: to_np(v)[col] for k, v in self.buffer[row].items()}
-
-#     def _get_batch(self, idxs):
-#         samples = [self[i] for i in idxs]
-#         batch = Batch.from_list_of_dicts(samples)
-#         batch = batch.apply_to_all(to_np)
-#         batch = batch.apply_to_all(lambda x: x[None])
-#         batch.idx = idxs
-#         return batch
-
-#     def add_sample(self, **kwargs):
-#         # If buffer is not full, add a new element
-#         if len(self.buffer) + self.num_envs <= self.maxlen:
-#             self.buffer.append(None)
-#         # Store new transition at the appropriate index
-#         self.position = (self.position + 1) % (self.maxlen // self.num_envs)
-#         self.buffer[self.position] = kwargs
-
-#     def sample(self, batch_size):
-#         idxs = np.random.choice(len(self), batch_size, replace=False)
-#         return self._get_batch(idxs=idxs)
-
-
 class ReplayBuffer:
     """
     Memory efficient implementation of replay buffer, storing each state only once.
@@ -106,6 +70,10 @@ class ReplayBuffer:
     @property
     def available_idxs(self):
         return self.num_envs * (len(self) - self.history_len - self.n_step + 1)
+
+    def reset(self):
+        self.current_idx = -1
+        self.current_len = 0
 
     def add_sample(self, state, action, reward, done):
         self.check_shapes(state, action, reward, done)
