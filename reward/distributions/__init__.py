e9687e4ba5be90a379a9c455594c95333a88b3ce
Lucas Vazquez <lucasgouvaz@gmail.com>
2018-11-13 01:43:43 -0200

    removed sigmoid from distrubutions init

--- reward/distributions/__init__.py
+++ reward/distributions/__init__.py
@@ -3,13 +3,5 @@ from .categorical import Categorical
 from .normal import Normal
 from .ornstein import Ornstein
 from .tanh_normal import TanhNormal
-from .sigmoid_normal import SigmoidNormal
 
-__all__ = [
-    "Categorical",
-    "Normal",
-    "BaseDist",
-    "Ornstein",
-    "TanhNormal",
-    "SigmoidNormal",
-]
+__all__ = ["Categorical", "Normal", "BaseDist", "Ornstein", "TanhNormal"]
