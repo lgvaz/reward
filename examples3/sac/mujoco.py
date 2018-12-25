0f69d3a87206b45e7ecb83edab42cca9f73ddd81
lgvaz <lucasgouvaz@gmail.com>
2018-12-25 01:16:26 -0200

    remove vnn

--- examples3/sac/mujoco.py
+++ examples3/sac/mujoco.py
@@ -32,22 +32,6 @@ class PolicyNN(nn.Module):
         log_std = self.log_std(x).clamp(*self.logstd_range)
         return mean, log_std
 
-
-class ValueNN(nn.Module):
-    def __init__(self, n_in, hidden=256, activation=nn.ReLU):
-        super().__init__()
-        layers = []
-        layers += [nn.Linear(n_in, hidden), activation()]
-        layers += [nn.Linear(hidden, hidden), activation()]
-        final_layer = nn.Linear(hidden, 1)
-        final_layer.weight.data.uniform_(-3e-3, 3e-3)
-        final_layer.bias.data.uniform_(-3e-3, 3e-3)
-        layers += [final_layer]
-        self.layers = nn.Sequential(*layers)
-
-    def forward(self, x): return self.layers(x)
-
-
 class QValueNN(nn.Module):
     def __init__(self, n_in, n_acs, hidden=256, activation=nn.ReLU):
         super().__init__()
@@ -61,7 +45,6 @@ class QValueNN(nn.Module):
         self.layers = nn.Sequential(*layers)
 
     def forward(self, s, a): return self.layers(torch.cat([s, a], dim=1))
-
 
 class Policy:
     def __init__(self, nn): self.nn = nn
@@ -94,19 +77,16 @@ a_map = U.map_range(-1, 1, A.low[0], A.high[0])
 pnn = PolicyNN(n_in=S.shape[0], n_out=A.shape[0]).to(DEVICE)
 q1nn = QValueNN(n_in=S.shape[0], n_acs=A.shape[0]).to(DEVICE)
 q2nn = QValueNN(n_in=S.shape[0], n_acs=A.shape[0]).to(DEVICE)
-vnn = ValueNN(n_in=S.shape[0]).to(DEVICE)
-vnn_targ = ValueNN(n_in=S.shape[0]).to(DEVICE).eval()
 policy = Policy(nn=pnn)
 
 p_opt = torch.optim.Adam(pnn.parameters(), lr=3e-4)
 q1_opt = torch.optim.Adam(q1nn.parameters(), lr=3e-4)
 q2_opt = torch.optim.Adam(q2nn.parameters(), lr=3e-4)
-v_opt = torch.optim.Adam(vnn.parameters(), lr=3e-4)
 
-rw.logger.set_logdir('logs/humanoid/autotemp-v5-0')
+rw.logger.set_logdir('logs/humanoid/autotemp-v2-6-0')
 rw.logger.set_maxsteps(20e6)
-model = rw.model.SAC2(policy=policy, q1nn=q1nn, q2nn=q2nn, vnn=vnn, vnn_targ=vnn_targ, p_opt=p_opt, q1_opt=q1_opt, q2_opt=q2_opt, v_opt=v_opt,
-                     r_scale=20., entropy=-17, gamma=0.99)
+entropy = -np.prod(env.action_space.shape)
+model = rw.model.SAC2(policy=policy, q1nn=q1nn, q2nn=q2nn, p_opt=p_opt, q1_opt=q1_opt, q2_opt=q2_opt, r_scale=20., entropy=entropy, gamma=0.99)
 agent = rw.agent.Replay(model=model, s_sp=S, a_sp=A, bs=256, maxlen=1e6)
 
 s = env.reset()
--- reward/model/sac2.py
+++ reward/model/sac2.py
@@ -1,68 +1,77 @@
 import torch, torch.nn as nn
 import torch.nn.functional as F
 import reward as rw, reward.utils as U
+from copy import deepcopy
 from .model import Model
 
 
 class SAC2(Model):
-    def __init__(self, *, policy, q1nn, q2nn, vnn, vnn_targ, p_opt, q1_opt, q2_opt, v_opt, r_scale, entropy, vnn_targ_w=0.005, gamma=0.99):
+    def __init__(self, *, policy, q1nn, q2nn, p_opt, q1_opt, q2_opt, r_scale, entropy, targ_smooth=0.005, gamma=0.99):
         super().__init__(policy=policy)
+        # TODO r_scale, what to do?
         self.ent_targ = entropy
         self.logtemp = nn.Parameter(torch.zeros(1).squeeze().to(U.device.get()))
+        # TODO: Auto lr
         self.temp_opt = self._wrap_opts(torch.optim.Adam([self.logtemp], lr=3e-4))
 
-        self.q1nn,self.q2nn,self.vnn,self.vnn_targ = q1nn,q2nn,vnn,vnn_targ
-        self.p_opt,self.q1_opt,self.q2_opt,self.v_opt = self._wrap_opts(p_opt,q1_opt,q2_opt,v_opt)
-        self.r_scale,self.vnn_targ_w,self.gamma = r_scale,vnn_targ_w,gamma
-        # Update value target nn
-        U.copy_weights(from_nn=self.vnn, to_nn=self.vnn_targ, weight=1.)
+        self.q1nn,self.q2nn = q1nn,q2nn
+        self.q1nn_targ, self.q2nn_targ = deepcopy(q1nn).eval(), deepcopy(q2nn).eval()
+        U.freeze_weights(self.q1nn_targ), U.freeze_weights(self.q2nn_targ)
+
+        self.p_opt,self.q1_opt,self.q2_opt = self._wrap_opts(p_opt,q1_opt,q2_opt)
+        self.targ_smooth = targ_smooth
+        self.r_scale,self.gamma = r_scale,gamma
+        self._update_targ_nn(w=1.)
 
     def train(self, *, ss, sns, acs, rs, ds):
         # (#samples, #envs, #feats) -> (#samples + #envs, #feats)
         ss, sns, acs = [[o.reshape((-1, *o.shape[2:])) for o in l] for l in [ss, sns, acs]]
         rs, ds = [o.reshape((-1, *o.shape[2:]))[..., None] for o in [rs, ds]]
         ### Sac update ###
-        q1b, q2b, vb = self.q1nn(*ss, *acs), self.q2nn(*ss, *acs), self.vnn(*ss)
-        dist = self.p.get_dist(*ss)
+        q1b, q2b = self.q1nn(*ss, *acs), self.q2nn(*ss, *acs)
+        dist, distn = self.p.get_dist(*ss), self.p.get_dist(*sns)
         anew, anew_pre = map(U.listify, self.p.get_act_pre(dist=dist))
+        anewn, anew_pren = map(U.listify, self.p.get_act_pre(dist=distn))
         # Divide log_prob by r_scale instead of multiplying reward, better stability
         logprob = self.p.logprob_pre(dist, *anew_pre)
-        logprob_scaled = logprob * self.logtemp.exp().detach()
-        assert logprob.shape == q1b.shape == q2b.shape == vb.shape == rs.shape == ds.shape
+        logprobn = self.p.logprob_pre(distn, *anew_pren)
+        logprob_scaled = logprob * self.temp.detach()
+        logprob_scaledn = logprobn * self.temp.detach()
+        # TODO: new shapes to assert
+        assert logprob.shape == q1b.shape == q2b.shape == rs.shape == ds.shape
         # Q loss
-        vtarg_tp1 = self.vnn_targ(*sns)
-        qt_next = U.estim.td_target(rs=rs, ds=ds, vn=vtarg_tp1, gamma=self.gamma)
-        q1_loss = (q1b - qt_next.detach()).pow(2).mean()
-        q2_loss = (q2b - qt_next.detach()).pow(2).mean()
-        # V Loss
-        q1new_t, q2new_t = self.q1nn(*ss, *anew), self.q2nn(*ss, *anew)
-        qnew_t = torch.min(q1new_t, q2new_t)
-        v_next = qnew_t - logprob_scaled
-        v_loss = F.mse_loss(vb, v_next.detach())
+        q1targn, q2targn = self.q1nn_targ(*sns, *anewn), self.q2nn_targ(*sns, *anewn)
+        qtargn = torch.min(q1targn, q2targn) - self.temp.detach() * logprobn
+        q_tdtarg = U.estim.td_target(rs=rs, ds=ds, vn=qtargn, gamma=self.gamma)
+        q1_loss = (q1b - q_tdtarg.detach()).pow(2).mean()
+        q2_loss = (q2b - q_tdtarg.detach()).pow(2).mean()
         # Policy loss
-        p_loss = (logprob_scaled - qnew_t).mean() 
+        q1new, q2new = self.q1nn(*ss, *anew), self.q2nn(*ss, *anew)
+        qnew = torch.min(q1new, q2new)
+        p_loss = (logprob_scaled - qnew).mean() 
         p_loss += 1e-3 * self.p.mean(dist=dist).pow(2).mean() + 1e-3 * self.p.std(dist=dist).log().pow(2).mean()
         # Temperature loss
-        # TODO: check shapes and broadcast
         temp_loss = -self.logtemp * (logprob + self.ent_targ).detach().mean()
         # Optimize
         self.q1_opt.optimize(loss=q1_loss, nn=self.q1nn)
         self.q2_opt.optimize(loss=q2_loss, nn=self.q2nn)
-        self.v_opt.optimize(loss=v_loss, nn=self.vnn)
         self.p_opt.optimize(loss=p_loss, nn=self.p.nn)
         self.temp_opt.optimize(loss=temp_loss)
-        # Update value target nn
-        U.copy_weights(from_nn=self.vnn, to_nn=self.vnn_targ, weight=self.vnn_targ_w)
+        self._update_targ_nn(w=self.targ_smooth)
         # Write logs
         rw.logger.add_log("policy/loss", p_loss)
-        rw.logger.add_log("v/loss", v_loss)
         rw.logger.add_log("q1/loss", q1_loss)
         rw.logger.add_log("q2/loss", q2_loss)
-        rw.logger.add_log('temperature', self.logtemp.exp(), precision=4)
+        rw.logger.add_log('temperature', self.temp, precision=4)
         rw.logger.add_histogram("policy/logprob", logprob)
         rw.logger.add_histogram("policy/mean", self.p.mean(dist=dist))
         rw.logger.add_histogram("policy/std", self.p.std(dist=dist))
-        rw.logger.add_histogram("v/value", vb)
         rw.logger.add_histogram("q1/value", q1b)
         rw.logger.add_histogram("q2/value", q2b)
 
+    @property
+    def temp(self): return self.logtemp.exp()
+
+    def _update_targ_nn(self, w):
+        U.copy_weights(from_nn=self.q1nn, to_nn=self.q1nn_targ, weight=w)
+        U.copy_weights(from_nn=self.q2nn, to_nn=self.q2nn_targ, weight=w)
