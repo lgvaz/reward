61f3d1eb3b83348cf8b99b5a1179dc53c80569f7
lgvaz <lucasgouvaz@gmail.com>
2018-12-25 22:13:59 -0200

    new meanstd filter

--- reward/utils/filter/mean_std_filter.py
+++ reward/utils/filter/mean_std_filter.py
@@ -1,144 +1,51 @@
-import warnings
 import numpy as np
-from reward.utils import EPSILON, to_np
+from reward.utils import EPSILON
 
 
 class MeanStdFilter:
-    """
-    Calculates the exact mean and std deviation, originally by `ray_rllib
-    <https://goo.gl/fMv49b>`_.
+    "Calculates the exact mean and std deviation, originally by `ray_rllib <https://goo.gl/fMv49b>`_."
+    def __init__(self, n_features, clip_range=np.inf):
+        if not isinstance(n_features, int): raise ValueError('n_features should be an int, got {}'.format(n_features))
+        self.n_features, self.clip_range, self.n, self.xs = n_features, clip_range, 0, []
+        self.M, self.S = np.zeros(n_features), np.zeros(n_features)
 
-    Parameters
-    ----------
-    shape: tuple
-        The shape of the inputs to :meth:`self.normalize` and :meth:`self.scale`
-    clip_range: float
-        The output of :meth:`self.normalize` and :meth:`self.scale` will be clipped
-        to this range, use np.inf for no clipping. (Default is 5)
-    """
+    def normalize(self, x, add_sample=True):
+        "Normalizes x by subtracting the mean and dividing by the standard deviation."
+        self._check_shape(x)
+        if add_sample: self.xs.extend(x)
+        return ((x - self.mean) / (self.std + EPSILON)).clip(-self.clip_range, self.clip_range)
 
-    def __init__(self, num_features, clip_range=5.):
-        # if len(shape) > 2:
-        #     warnings.warn(
-        #         UserWarning('Input shape should be in the form (num_envs, num_features), '
-        #                     'might not work as expected with different shapes.'))
-        if not isinstance(num_features, int):
-            raise ValueError(
-                "num_features should be an int, got {}".format(num_features)
-            )
-
-        self.num_features = num_features
-        self.clip_range = clip_range
-        self.n = 0
-        self.xs = []
-
-        self.M = np.zeros(num_features)
-        self.S = np.zeros(num_features)
-
-    def _norm(self, use_latest):
-        mean_copy = None if use_latest else self.mean.copy()
-        std_copy = None if use_latest else self.std.copy()
-
-        def apply(x):
-            mean = self.mean if use_latest else mean_copy
-            std = self.std if use_latest else std_copy
-            return ((x - mean) / (std + EPSILON)).clip(
-                min=-self.clip_range, max=self.clip_range
-            )
-
-        return apply
-
-    def _scale(self, use_latest):
-        std_copy = None if use_latest else self.std.copy()
-
-        def apply(x):
-            std = self.std if use_latest else std_copy
-            return (x / (std + EPSILON)).clip(min=-self.clip_range, max=self.clip_range)
-
-        return apply
-
-    def _check_shape(self, x):
-        if not (self.num_features,) == x.shape[1:]:
-            raise ValueError(
-                "Data shape must be (num_samples, {}) but is {}".format(
-                    self.num_features, x.shape
-                )
-            )
-
-    @property
-    def mean(self):
-        return self.M
-
-    @property
-    def var(self):
-        if self.n == 0 or self.n == 1:
-            return np.ones(self.S.shape)
-        else:
-            return self.S / (self.n - 1)
-
-    @property
-    def std(self):
-        return np.sqrt(self.var)
+    def scale(self, x, add_sample=True):
+        "Scales x by dividing by the standard deviation."
+        self._check_shape(x)
+        if add_sample: self.xs.extend(x)
+        return (x / (self.std + EPSILON)).clip(-self.clip_range, self.clip_range)
 
     def update(self):
-        n_old = self.n
-        n_new = len(self.xs)
-        if n_new == 0:
-            return
-
-        x = to_np(self.xs)
+        n_old, n_new = self.n, len(self.xs)
+        if n_new == 0: return
+        x, self.xs = np.array(self.xs), []
         self.n += n_new
-        self.xs = []
-
         x_mean = x.mean(axis=0)
-        x_std = ((x - x_mean) ** 2).sum(axis=0)
+        x_std  = ((x - x_mean) ** 2).sum(axis=0)
         # First update
         if self.n == n_new:
-            self.M[:] = x_mean
             self.S[:] = x_std
+            self.M[:] = x_mean
         else:
             new_mean = (n_old * self.M + n_new * x_mean) / self.n
             self.S[:] = self.S + x_std + (self.M - x_mean) ** 2 * n_old * n_new / self.n
             self.M[:] = new_mean
 
-    def normalize(self, x, add_sample=True):
-        """
-        Normalizes x by subtracting the mean and dividing by the standard deviation.
+    @property
+    def mean(self): return self.M
+    @property
+    def std(self): return np.sqrt(self.var)
+    @property
+    def var(self):
+        if self.n == 0 or self.n == 1: return np.ones(self.S.shape)
+        else:                          return self.S / (self.n - 1)
 
-        Parameters
-        ----------
-        add_sample: bool
-            If True x will be added as a new sample and will be considered when
-            the filter is updated via :meth:`self.update`. (Default is True)
-
-        Returns
-        -------
-        np.ndarray
-        """
-        self._check_shape(x)
-        if add_sample:
-            self.xs.extend(x)
-        value = (x - self.mean) / (self.std + EPSILON)
-        value = value.clip(min=-self.clip_range, max=self.clip_range)
-        return value
-
-    def scale(self, x, add_sample=True):
-        """
-        Scales x by dividing by the standard deviation.
-
-        Parameters
-        ----------
-        add_sample: bool
-            If True x will be added as a new sample and will be considered when
-            the filter is updated via :meth:`self.update`. (Default is True)
-
-        Returns
-        -------
-        np.ndarray
-        """
-        self._check_shape(x)
-        if add_sample:
-            self.xs.extend(x)
-        value = x / (self.std + EPSILON)
-        value = value.clip(min=-self.clip_range, max=self.clip_range)
-        return value
+    def _check_shape(self, x):
+        if not (self.n_features,) == x.shape[1:]:
+            raise ValueError(f'Data shape must be (num_samples, {self.n_features}) but is {x.shape}')
